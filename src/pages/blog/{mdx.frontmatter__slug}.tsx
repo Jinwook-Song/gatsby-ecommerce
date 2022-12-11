ba7e917f02e76bb2107687ee0bff91a0635d10fa
jinwook song <wlsdnr129@naver.com>
2022-12-11 22:14:38 +0900

    feat: gatsbyImage

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,4 +1,5 @@
 import { graphql, HeadFC, PageProps } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import React from 'react';
 import Layout from '../../components/Layout';
 import Seo from '../../components/Seo';
@@ -9,9 +10,12 @@ interface IBlogPostProps {
 }
 
 function BlogPost({ data, children }: IBlogPostProps) {
-  console.log(data, children);
+  const image = getImage(
+    data.mdx?.frontmatter?.headerImage?.childImageSharp?.gatsbyImageData!
+  );
   return (
     <Layout title='Blog Post'>
+      <GatsbyImage image={image!} alt={data.mdx?.frontmatter?.title!} />
       <div>{children}</div>
     </Layout>
   );
@@ -22,13 +26,17 @@ export default BlogPost;
 export const query = graphql`
   query PostDetail($frontmatter__slug: String) {
     mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
-      body
       frontmatter {
         author
         category
         date
         slug
         title
+        headerImage {
+          childImageSharp {
+            gatsbyImageData(height: 450, placeholder: BLURRED)
+          }
+        }
       }
     }
   }
