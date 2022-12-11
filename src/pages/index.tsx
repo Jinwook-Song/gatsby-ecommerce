1a743af76e1d9095e05c54eb461a73be5f7c1aa1
jinwook song <wlsdnr129@naver.com>
2022-12-11 21:50:41 +0900

    feat: static images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,16 @@ import * as React from 'react';
 import { HeadFC, PageProps } from 'gatsby';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout title='Welcome to DevStickers 🦄'>
       <div></div>
+      <StaticImage
+        src='https://images.unsplash.com/photo-1625768376503-68d2495d78c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2825&q=80'
+        alt='Stickers on the wall'
+      />
     </Layout>
   );
 };
